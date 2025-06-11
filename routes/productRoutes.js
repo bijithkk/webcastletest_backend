25fd459ad2ce8311d37e96bd84f2ee843303a2ac
bijithkk <informbijith@gmail.com>
2025-06-11 20:14:33 +0530

    routes created

---
+++ routes/productRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const upload = require("../middleware/multer");
+const productController = require('../controllers/productController');
+
+//create new product
+router.post("/add", upload.single("image"), productController.createProduct);
+
+// get all products
+router.get("/get",productController.getAllProducts);
+
+// get product by id
+router.get("/get/:id",productController.getProductById);
+
+// update product by id
+router.patch("/update/:id",upload.single("image"),productController.updateProductById);
+
+// delete product by id
+router.delete("/delete/:id", productController.deleteProductById);
+
+module.exports = router;
