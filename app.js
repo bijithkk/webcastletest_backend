be2111d25bebce191662f4a0f8c48eb25bfee693
bijithkk <informbijith@gmail.com>
2025-06-14 13:45:00 +0530

    added get/ route

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 //Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.use('/api/v1/product', productRouter);
 
 module.exports = app;
